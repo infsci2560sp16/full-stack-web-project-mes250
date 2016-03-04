7e30aafd22604ef1908166f4eb460678f3e9c121
mes250 <mes250@pitt.edu>
2016-03-04 15:30:21 -0500

    Changed JS

--- src/main/resources/public/js/invControl.js
+++ src/main/resources/public/js/invControl.js
@@ -16,46 +16,6 @@ function handleData(data) {
 }
 
 
-function generateTable() {
-	//Create a HTML Table element
-	var table = document.createElement("TABLE");
-	table.border = "1";
-	table.id = "inventory";
-	table.summary = "This table lists all devices in the inventory";
-
-	//Get the count of columns
-	var columnCount = inventory[0].length;
-
-	//Add the header row
-	var row = table.insertRow(-1);
-	var headerCell = table.createTHead();
-	
-	
-	//Insert data for each header column
-	for (var i = 0; i < columnCount; i++) {
-		headerCell = document.createElement("TH");
-		headerCell.innerHTML = inventory[0][i];
-		row.appendChild(headerCell);
-	}
-	
-	
-	
-	//Add the data rows
-	for (i = 1; i < inventory.length; i++) {
-		var row2 = table.insertRow(-1);
-		
-		//add data cells
-		for (var j = 0; j < columnCount; j++) {
-			var cell = row2.insertCell(-1);
-			cell.innerHTML = inventory[i][j] ;
-		}
-	}
-
-	var invTable = document.getElementById("invTable");
-	invTable.innerHTML = "";
-	invTable.appendChild(table);
-} //end Generate Table
-
 function generateTableRetire() {
 	//Create a HTML Table element
 	var table = document.createElement("TABLE");
@@ -151,6 +111,7 @@ function generateTableDel() {
 function insert(){
 	//Pull new data from form
 	var ownerValue = document.getElementById('Owner').value;
+	var devicenameValue = document.getElementByID('DeviceName').value;
 	var manufacturerValue = document.getElementById('Manufacturer').value;
 	var modelValue = document.getElementById('Model').value;
 	var typeValue = document.getElementById('Type').value;
