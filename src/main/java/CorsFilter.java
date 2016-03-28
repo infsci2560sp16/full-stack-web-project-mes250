54c0712d2ddf9ebe9caefa3a3b38c2d3f31a214c
mes250 <mes250@pitt.edu>
2016-03-28 17:28:19 -0400

    Modified CORS

--- src/main/java/CorsFilter.java
+++ src/main/java/CorsFilter.java
@@ -18,7 +18,8 @@ public class CorsFilter {
      private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
     
     static {
-        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        //corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
         corsHeaders.put("Access-Control-Allow-Origin", "http://stark-earth-7570.herokuapp.com");
         //corsHeaders.put("Access-Control-Allow-Origin", "http://localhost:5000");
         corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
